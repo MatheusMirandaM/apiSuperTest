1f823750278f5ffb0f84a7583b92cc04dbf68e41
MatheusMirandaM <matheusm7web@gmail.com>
2023-11-25 22:30:35 -0300

    Oi Deus, sou eu de novo

---
+++ api/petCrud.spec.js
@@ -0,0 +1,73 @@
+// Biblioteca
+const supertest = require('supertest'); // Framework de teste de API
+const assert = require('chai').assert; // Função de assertiva do resultado
+
+const baseUrl = 'https://petstore.swagger.io/v2'; // URL base
+const petId = 11730189; // id do animal
+
+describe('PetStore Swegger - Pet', () => {
+    
+    const request = supertest(baseUrl);
+
+    // Inserir um registro
+    it('POST Pet', () => {
+
+        const jsonFile = require('../json/pet.json'); // Apontamento para o arquivo com o json 
+
+        return request          // chama a requisição
+            .post('/pet')       // endpoint (path))
+            .send(jsonFile)     // envia os dados no corpo da requisição
+            .then((response) => {
+                assert.equal(response.statusCode, 200);
+                assert.equal(response.body.id, petId);
+                assert.equal(response.body.name, "Kira");
+                assert.equal(response.body.status, "available");
+            });
+    
+    });
+
+    // Consultar registro
+    it('GET - Pet', () => {
+        
+        return request
+            .get(`/pet/${petId}`)
+            .then((response) => {
+                assert.equal(response.statusCode, 200);
+                assert.equal(response.body.id, petId);
+                assert.equal(response.body.name, "Kira");
+                assert.equal(response.body.status, "available");
+            });
+
+    });
+
+    // Atualizar registro
+    it('PUT - Pet', () => {
+
+        const jsonFile = require('../json/petPUT.json');
+        
+        return request
+            .put('/pet')
+            .send(jsonFile)
+            .then((response) => {
+                assert.equal(response.statusCode, 200);
+                assert.equal(response.body.id, petId);
+                assert.equal(response.body.name, 'Kira');
+                assert.equal(response.body.tags[1].id, 4);
+                assert.equal(response.body.tags[1].name, "castrated");
+                assert.equal(response.body.status, "solded");
+            });
+
+    });
+
+    // Deletar registro
+    it('DELETE - Pet', () => {
+
+        return request
+            .delete(`/pet/${petId}`)
+            .then((response) => {
+                assert.equal(response.statusCode, 200);
+            });
+        
+    });
+
+});
